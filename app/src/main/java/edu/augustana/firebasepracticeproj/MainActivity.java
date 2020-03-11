07edcd44db8d1d8227c4b495c07da4756dc6e355
brysonadcock22 <brysonadcock16@augustana.edu>
2020-03-11 17:57:36 -0500

    Second commit

--- app/src/main/java/edu/augustana/firebasepracticeproj/MainActivity.java
+++ app/src/main/java/edu/augustana/firebasepracticeproj/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class MainActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
     }
+
+
 }
